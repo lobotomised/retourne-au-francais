542b0e3d417e42f8e9a2e0658d9750b986dc3a47
lobotomised <lobotomised@lobotomised.lobotomised>
2025-06-17 09:45:52 +0200

    Ajoute un MutationObserver pour gérer les contenus dynamiques

--- scripts/app.js
+++ scripts/app.js
@@ -13,3 +13,21 @@ new RetourneFrancais()
     .rules(rules)
     .text(document.body)
     .run()
+
+const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+        for (const node of mutation.addedNodes) {
+            if (node.nodeType === Node.TEXT_NODE) {
+                new RetourneFrancais().rules(rules).text(node).run()
+            } else if (node.nodeType === Node.ELEMENT_NODE) {
+                new RetourneFrancais().rules(rules).text(node).run()
+            }
+        }
+    }
+})
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    characterData: true,
+})
