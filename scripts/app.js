92c6d7a1d1fd458303a55f01aefc91f773ed6571
lobotomised <lobotomised@lobotomised.lobotomised>
2025-06-17 09:47:05 +0200

    Support les formats: foo.es foo·es foo-es

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
 const rules = [
     { pattern: /\.e\.s\b/g, replacement: 's' },
+    { pattern: /\.es\b/g, replacement: 's' },
     { pattern: /\.e\b/g, replacement: '' },
 
     { pattern: /·e·s\b/g, replacement: 's' },
+    { pattern: /·es\b/g, replacement: 's' },
     { pattern: /·e\b/g, replacement: '' },
 
     { pattern: /-e-s\b/g, replacement: 's' },
+    { pattern: /-es\b/g, replacement: 's' },
     { pattern: /-e\b/g, replacement: '' },
 ]
 
